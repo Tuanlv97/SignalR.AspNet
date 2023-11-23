55c968c42d5e93a8acf11a5d1df76fb8ef9aaf56
Thợ code <tuanlv2@vhv.vn>
2023-11-23 17:46:57 +0700

    Init-Project

---
+++ SignalRServer/Hubs/MyHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServer.Hubs
+{
+    public class MyHub : Hub
+    {
+        private static List<string> Names { get; set; } = new List<string>();
+        private static int ClientCount { get; set; } = 0;
+
+        public async Task SendName(string name)
+        {
+            Names.Add(name);
+            await Clients.All.SendAsync("ReceiveName", name);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            ClientCount++;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnConnectedAsync();
+        }
+
+        public async override Task OnDisconnectedAsync(Exception? exception)
+        {
+            ClientCount--;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnDisconnectedAsync(exception); 
+        }
+
+    }
+}
